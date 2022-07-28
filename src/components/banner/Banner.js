9674f1c17c30bba127033fc20b9e71e48e267287
unknown <ccord042@fiu.edu>
2022-07-28 02:25:53 -0400

    router fix

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -5,11 +5,11 @@ function Banner() {
     return (
         <div className="banner-container">
         <div className="left-sliding">
-        <marquee class = "marquee" behavior= "scroll" direction="left" scrollamount = "15" height = "20px">
+        <marquee class = "marquee" behavior= "scroll" direction="left" scrollamount = "10" height = "20px">
            MIAMI BASED ASPIRING SOFTWARE ENGINEER* / FRONT END DEVELOPER* </marquee>
            <div/>
            <div className="right-sliding">
-           <marquee class = "marquee" behavior="scroll" direction="right"  scrollamount = "20" height = "20px">
+           <marquee class = "marquee" behavior="scroll" direction="right"  scrollamount = "12" height = "20px">
             REACT // JAVA // HTML // CSS // CREATIVE DESIGN // FRONT END // BACK END // </marquee>
             </div>
         </div>

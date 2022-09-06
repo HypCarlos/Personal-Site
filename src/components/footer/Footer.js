91cb231ada3186d4184db7e5d0a72d5819a5c4fa
unknown <ccord042@fiu.edu>
2022-09-06 10:11:58 -0400

    footer changes

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,12 +7,7 @@ function Footer() {
       <div className="footer-info">
         <div className="footer-icons">
           {/* <h4>Follow me.</h4> */}
-          <a href="https://twitter.com/OgPesci">
-            <img
-              src="https://img.icons8.com/material-rounded/24/000000/twitter.png"
-              alt="twitter"
-            ></img>
-          </a>
+          
           <a href="https://github.com/HypCarlos">
             <img
               src="https://img.icons8.com/material-outlined/24/000000/github.png"

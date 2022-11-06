442e05595c65484231a750103c3f8808a11f667d
unknown <ccord042@fiu.edu>
2022-11-06 13:47:10 -0500

    changes to bg and contact wrapper

--- src/data.js
+++ src/data.js
@@ -2,22 +2,23 @@ export const projects = [
 
     {
         id: 3,
-        img: require('../src/img/ecom.png'),
+        img: require('./assets/img/ecom.png'),
         link: 'https://github.com/HypCarlos/E-Commerce-Clone',
         name: 'E-Commerce site'
     },
     {
         id: 1,
-        img: require("./img/father.png"),
+        img: require("./assets/img/father.png"),
         link: 'https://fatherholdmynotepad.com/',
         name: 'Kanye Meme'
     },
     {
         id: 2,
-        img: require("./img/patentify.png"),
+        img: require("./assets/img/patentify.png"),
         link: 'https://patentify.cognac.cs.fiu.edu/Home',
         name: 'Capstone Project'
-    }
+    },
+  
    
  
 ]

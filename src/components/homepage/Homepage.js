a55e60ff48041e41c0748c15096cd8b7dcf10323
unknown <ccord042@fiu.edu>
2023-01-11 22:48:47 -0500

    3d model update

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -19,8 +19,8 @@ function Homepage() {
         </div>
 
         <div className="header-content">
-        <Spline id = "spline" scene="https://prod.spline.design/kIiw1s0Q4YKhvi0s/scene.splinecode" />
-        {/* <Spline id = "spline" scene="https://prod.spline.design/bLvvJN7hGGFc-Wfb/scene.splinecode" /> */}
+        <Spline scene="https://prod.spline.design/tCQ6KR1ZlaaoKBy7/scene.splinecode" />
+
         {/* <Spline id="spline" scene="https://prod.spline.design/U5OABLgXnOgXNjhW/scene.splinecode" /> */}
 
         {/* <Spline id = "spline" scene="https://prod.spline.design/XpQZCA1OCD8cSVd9/scene.splinecode" /> */}

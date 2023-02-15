386433f0b2e5a3725de5019e405393761f8d3caf
unknown <ccord042@fiu.edu>
2023-02-14 21:39:53 -0500

    changes to hero section

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -3,10 +3,9 @@ import "../homepage/homepage.css";
 import Navbar from "../navbar/Navbar";
 import Projects from "../projects/Projects";
 import Contact from "../contact/Contact";
-import Spline from '@splinetool/react-spline';
+import Spline from "@splinetool/react-spline";
 import Banner from "../banner/Banner";
-
-
+import TypeWriterEffect from "react-typewriter-effect";
 
 //COMPONENTS
 
@@ -19,26 +18,38 @@ function Homepage() {
         </div>
 
         <div className="header-content">
-        
+          <div className="header-left">
+            <h1 id="text">
+              {" "}
+              <TypeWriterEffect
+                color="white"
+                startDelay={0}
+                cursorColor="white"
+                multiText={["Hello!", "its", "[Carlos A. Cordero]"]}
+                typeSpeed={80}
+                loop={true}
+              />
+            </h1>
+          </div>
+          <div className="header-right">
+            <Spline scene="https://prod.spline.design/tCQ6KR1ZlaaoKBy7/scene.splinecode" />
+          </div>
+          {/* <Spline scene="https://prod.spline.design/8hHGoxrB4NPFePK8/scene.splinecode" /> */}
 
-        {/* <Spline scene="https://prod.spline.design/8hHGoxrB4NPFePK8/scene.splinecode" /> */}
-        <Spline scene="https://prod.spline.design/tCQ6KR1ZlaaoKBy7/scene.splinecode" />
+          {/* <Spline id="spline" scene="https://prod.spline.design/U5OABLgXnOgXNjhW/scene.splinecode" /> */}
 
-        {/* <Spline id="spline" scene="https://prod.spline.design/U5OABLgXnOgXNjhW/scene.splinecode" /> */}
-
-        {/* <Spline id = "spline" scene="https://prod.spline.design/XpQZCA1OCD8cSVd9/scene.splinecode" /> */}
+          {/* <Spline id = "spline" scene="https://prod.spline.design/XpQZCA1OCD8cSVd9/scene.splinecode" /> */}
         </div>
       </div>
       <div className="banner-container">
-        <Banner/>
+        <Banner />
       </div>
-      <div id = "projects" className="projects-container">
+      <div id="projects" className="projects-container">
         <Projects />
       </div>
-      <div id = "contact" className="contact-container">
+      <div id="contact" className="contact-container">
         <Contact />
       </div>
-    
     </div>
   );
 }
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -5,43 +5,20 @@ import TypeWriterEffect from "react-typewriter-effect";
 function Navbar() {
   return (
     <div className="navbar-container">
-      <div className="left-half">
-        <div className="navbar-header">
-       <h1 id= "text" > <TypeWriterEffect
-            color = "white"
-            startDelay={0}
-            cursorColor="white"
-
-            multiText={[
-              "Hello!",
-              "its",
-              "[Carlos A. Cordero]",
-            ]}
-            typeSpeed={80}
-            loop={true}
-          />
-          </h1>
-        
-        </div>
-      </div>
-      <div className="right-half">
-        <div className="nav-buttons">
-          <ul>
-            <li>
-              <a href="#projects">Work</a>
-            </li>
-            <li>
-              {" "}
-              <a href="/">About</a>
-            </li>
-            <li>
-              {" "}
-              <a href="#contact">Contact</a>
-            </li>
-          </ul>
-          
-        </div>
-        
+      <div className="nav-buttons">
+        <ul>
+          <li>
+            <a href="#projects">Work</a>
+          </li>
+          <li>
+            {" "}
+            <a href="/">About</a>
+          </li>
+          <li>
+            {" "}
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
       </div>
     </div>
   );
